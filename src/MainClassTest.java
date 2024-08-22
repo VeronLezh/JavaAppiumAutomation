9ccf1836cb9d8464b47752de275294ac0402faad
Veronika Lezhneva <veronikalezhneva@MacBook-Air-Veronika.local>
2024-08-22 10:59:23 +0300

    Lesson1 Test2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -6,9 +6,13 @@ import org.junit.Test;
 public class MainClassTest extends MainClass {
 
     @Test
-
     public void testGetLocalNumber(){
+
         Assert.assertEquals("Number is not equal 14", 14, getLocalNumber());
     }
 
+    @Test
+    public void testGetClassNumber(){
+        Assert.assertTrue("Number should be more than 20", getClassNumber()>45);
+    }
 }
